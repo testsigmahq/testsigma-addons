6bcd65174b795353d196c1e16db7f7b6a00d871e
Sivasai Podugu <sivasai.podugu@testsigma.com>
2024-07-24 14:09:54 +0530

    Feat[TE-20444]: Added required nlps in mobile_web upgraded addon to 1.0.8

---
+++ image_based_actions/src/main/java/com/testsigma/addons/mobile_web/SearchImageWithScale.java
@@ -0,0 +1,40 @@
+package com.testsigma.addons.mobile_web;
+
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.OCR;
+import com.testsigma.sdk.annotation.TestData;
+import com.testsigma.sdk.annotation.TestStepResult;
+import lombok.Data;
+import org.openqa.selenium.NoSuchElementException;
+
+@Data
+@Action(actionText = "Verify if image image-url is present in current-page with search threshold threshold-value (Ex: 0.9 , means 90% match) and scale as scale-factor",
+        description = "Verify if the given image with threshold and scale is present in the current page",
+        applicationType = ApplicationType.MOBILE_WEB)
+public class SearchImageWithScale extends com.testsigma.addons.web.SearchImageWithScale {
+    @TestData(reference = "image-url")
+    private com.testsigma.sdk.TestData testData1;
+    @TestData(reference = "threshold-value")
+    private com.testsigma.sdk.TestData testData2;
+    @TestData(reference = "scale-factor")
+    private com.testsigma.sdk.TestData testData3;
+
+
+    @OCR
+    private com.testsigma.sdk.OCR ocr;
+
+    @TestStepResult
+    private com.testsigma.sdk.TestStepResult testStepResult;
+
+    @Override
+    protected Result execute() throws NoSuchElementException {
+        super.setTestData1(testData1);
+        super.setTestData2(testData2);
+        super.setTestData3(testData3);
+        super.setOcr(ocr);
+        super.setTestStepResult(testStepResult);
+        return super.execute();
+    }
+}
