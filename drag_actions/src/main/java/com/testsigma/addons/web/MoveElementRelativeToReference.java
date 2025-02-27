322e4ed60b8fb12162bcec8707c5eef708c602a7
Manoj <manojkumar.darshankar@testsigma.com>
2025-02-27 16:57:24 +0530

    Added "center" option in allowed values for 2 NLP's

--- drag_actions/src/main/java/com/testsigma/addons/web/MoveElementRelativeToReference.java
+++ drag_actions/src/main/java/com/testsigma/addons/web/MoveElementRelativeToReference.java
@@ -1,5 +1,6 @@
 package com.testsigma.addons.web;
 
+
 import com.testsigma.sdk.ApplicationType;
 import com.testsigma.sdk.WebAction;
 import com.testsigma.sdk.annotation.Action;
@@ -11,14 +12,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.MoveTargetOutOfBoundsException;
 
-import java.time.Duration;
-
-
-@Action(actionText = "Drag element elementLocator to the relativePosition of the element referenceElement with " +
-        "offset x: xOffset , y: yOffset (with pause)",
+@Action(actionText = "Drag element elementLocator to the relativePosition of the referenceElement with " +
+        "offset x: xOffset , y: yOffset",
         applicationType = ApplicationType.WEB,
         useCustomScreenshot = false)
-public class MoveElementRelativeToReferenceWithPause extends WebAction {
+public class MoveElementRelativeToReference extends WebAction {
 
     @Element(reference = "elementLocator")
     private com.testsigma.sdk.Element targetElement;
@@ -32,7 +30,7 @@ public class MoveElementRelativeToReferenceWithPause extends WebAction {
     @TestData(reference = "yOffset")
     private com.testsigma.sdk.TestData yOffsetData;
 
-    @TestData(reference = "relativePosition", allowedValues = {"right", "left", "top", "bottom", "center"})
+    @TestData(reference = "relativePosition", allowedValues = {"right", "left", "top", "bottom","center"})
     private com.testsigma.sdk.TestData relativePositionData;
 
     @Override
@@ -40,37 +38,24 @@ public class MoveElementRelativeToReferenceWithPause extends WebAction {
         logger.info("Execution started");
         com.testsigma.sdk.Result result = com.testsigma.sdk.Result.SUCCESS;
 
-        int targetXOffset = -1;
-        int targetYOffset = -1;
+        int xOffset = Integer.parseInt(xOffsetData.getValue().toString());
+        int yOffset = Integer.parseInt(yOffsetData.getValue().toString());
+        logger.info("xOffset: " + xOffset + " yOffset: " + yOffset);
+        String relativePosition = relativePositionData.getValue().toString().toLowerCase();
+        logger.info("relativePosition: " + relativePosition);
+        int targetXOffset=-1; //initializing values so that we can use in error message
+        int targetYOffset=-1;
         try {
-            int xOffset = Integer.parseInt(xOffsetData.getValue().toString());
-            int yOffset = Integer.parseInt(yOffsetData.getValue().toString());
-            logger.info("xOffset: " + xOffset + " yOffset: " + yOffset);
-            String relativePosition = relativePositionData.getValue().toString().toLowerCase();
-            logger.info("relativePosition: " + relativePosition);
-            logger.info("getting target element");
-            //  WebElement referenceWebElement = referenceElement.getElement();
-            WebElement referenceWebElement = driver.findElement(referenceElement.getBy());
-            logger.info("referenceWebElement: ");
-            //  WebElement targetWebElement = targetElement.getElement();
-            WebElement targetWebElement = driver.findElement(targetElement.getBy());
-            logger.info("got referenceWebElement ");
+            WebElement referenceWebElement = referenceElement.getElement();
+            WebElement targetWebElement = targetElement.getElement();
+
             targetXOffset = calculateXOffset(referenceWebElement, relativePosition, xOffset);
             targetYOffset = calculateYOffset(referenceWebElement, relativePosition, yOffset);
             logger.info("Target X Offset: " + targetXOffset + " Target Y Offset: " + targetYOffset);
             Actions actions = new Actions(driver);
-            logger.info("dummy click on target element");
-            targetWebElement.click();
-            actions.clickAndHold(targetWebElement).release().perform();
-            logger.info("Activated target element");
             actions.moveToElement(targetWebElement)
                     .clickAndHold()
-                    .pause(500) // adding pause to enable dragging
                     .moveToLocation(targetXOffset, targetYOffset)
-                    .pause(Duration.ofMillis(200))
-                    .moveByOffset(5, 5) // adding temporary move by offset so that the element is dropped
-                    .pause(200)
-                    .moveByOffset(-5, -5)
                     .release().build().perform();
             logger.info("Moved element");
             setSuccessMessage("Moved element to the " + relativePosition + " of the reference element");
@@ -79,20 +64,17 @@ public class MoveElementRelativeToReferenceWithPause extends WebAction {
             logger.info(ExceptionUtils.getStackTrace(e));
             result = com.testsigma.sdk.Result.FAILED;
         } catch (MoveTargetOutOfBoundsException e) {
-            setErrorMessage("Target location" + targetXOffset + "," + targetYOffset + "is out of bounds: " + e.getMessage());
+            setErrorMessage("Target location" + targetXOffset +"," + targetYOffset +"is out of bounds: " + e.getMessage());
             logger.info(ExceptionUtils.getStackTrace(e));
             result = com.testsigma.sdk.Result.FAILED;
         } catch (Exception e) {
-            setErrorMessage(ExceptionUtils.getStackTrace(e));
-
+            setErrorMessage(e.getMessage());
             logger.info(ExceptionUtils.getStackTrace(e));
             result = com.testsigma.sdk.Result.FAILED;
         }
         return result;
     }
-
     private int calculateXOffset(WebElement referenceElement, String relativePosition, int xOffset) {
-        logger.info("calculating xOffset ");
         int referenceX = referenceElement.getLocation().getX();
         int referenceWidth = referenceElement.getSize().getWidth();
         logger.info("referenceX: " + referenceX + " referenceWidth: " + referenceWidth);
@@ -109,7 +91,6 @@ public class MoveElementRelativeToReferenceWithPause extends WebAction {
     }
 
     private int calculateYOffset(WebElement referenceElement, String relativePosition, int yOffset) {
-        logger.info("calculating yOffset ");
         int referenceY = referenceElement.getLocation().getY();
         int referenceHeight = referenceElement.getSize().getHeight();
         logger.info("referenceY: " + referenceY + " referenceHeight: " + referenceHeight);
@@ -125,4 +106,3 @@ public class MoveElementRelativeToReferenceWithPause extends WebAction {
         }
     }
 }
-
