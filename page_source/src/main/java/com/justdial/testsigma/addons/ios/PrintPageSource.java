572134c6f237cfc26d8717bc207f4abd4cf89919
ManojTestsigma <manojkumar.darshankar@testsigma.com>
2025-02-28 12:33:54 +0530

    MOVED existing addon to indian region (#115)

---
+++ page_source/src/main/java/com/justdial/testsigma/addons/ios/PrintPageSource.java
@@ -0,0 +1,38 @@
+package com.justdial.testsigma.addons.ios;
+
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.IOSAction;
+import com.testsigma.sdk.annotation.Action;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchElementException;
+
+@Data
+@Action(actionText = "Print Page Source in log",
+        description = "Print Page Source in log",
+        applicationType = ApplicationType.IOS,
+        useCustomScreenshot = false)
+public class PrintPageSource extends IOSAction {
+
+
+    @Override
+    public com.testsigma.sdk.Result execute() throws NoSuchElementException {
+        //Your Awesome code starts here
+        logger.info("Initiating execution");
+
+        com.testsigma.sdk.Result result = com.testsigma.sdk.Result.SUCCESS;
+        try {
+            logger.info("Printing page source");
+            logger.info(driver.getPageSource());
+            setSuccessMessage("Successfully printed page source to log. You can verify the log file under additional NLP Logs");
+            return result;
+
+        } catch (Exception error) {
+            result = com.testsigma.sdk.Result.FAILED;
+            logger.info(" Unable to print page source:" + ExceptionUtils.getStackTrace(error));
+            setErrorMessage("Unable to print page source:" + error.getMessage());
+            return result;
+        }
+
+    }
+}
