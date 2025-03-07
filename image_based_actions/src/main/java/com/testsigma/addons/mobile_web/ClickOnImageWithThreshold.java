00099df9719de90dc4e059cc0970860e97419325
akhil-testsigma <appini.akhil@testsigma.com>
2025-03-08 00:13:33 +0530

    Added-occurence-based-code-for-already-present-nlp-(ClickOnTextSystemOrDesktopOccurenceBased.java)

---
+++ image_based_actions/src/main/java/com/testsigma/addons/mobile_web/ClickOnImageWithThreshold.java
@@ -0,0 +1,71 @@
+package com.testsigma.addons.mobile_web;
+
+import com.google.common.collect.ImmutableMap;
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.WebAction;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.OCR;
+import com.testsigma.sdk.annotation.TestData;
+import com.testsigma.sdk.annotation.TestStepResult;
+import io.appium.java_client.AppiumDriver;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.DriverCommand;
+
+import java.io.File;
+
+import static com.testsigma.addons.mobile_web.util.ClickActionUtils.performClick;
+import static com.testsigma.addons.mobile_web.util.ContextUtils.getCurrentContext;
+
+@Data
+@Action(actionText = "Click on image image-file with applied search threshold threshold-value (Ex: 0.9 , means 90% match)",
+        description = "Click on given image with threshold",
+        applicationType = ApplicationType.MOBILE_WEB)
+public class ClickOnImageWithThreshold extends WebAction {
+    @TestData(reference = "image-file")
+    private com.testsigma.sdk.TestData testData1;
+    @TestData(reference = "threshold-value")
+    private com.testsigma.sdk.TestData testData2;
+
+
+
+    @OCR
+    private com.testsigma.sdk.OCR ocr;
+
+    @TestStepResult
+    private com.testsigma.sdk.TestStepResult testStepResult;
+
+    @Override
+    protected Result execute() throws NoSuchElementException {
+        AppiumDriver appiumDriver = (AppiumDriver) driver;
+        String existingContext = getCurrentContext(appiumDriver);
+        try {
+            TakesScreenshot scrShot = ((TakesScreenshot) driver);
+            File baseImageFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+            // Switch to native app context
+            appiumDriver.execute(DriverCommand.SWITCH_TO_CONTEXT, ImmutableMap.of("name", "NATIVE_APP"));
+
+            String successMessage = performClick(ocr, testData1.getValue().toString(),
+                    Float.valueOf(testData2.getValue().toString()), baseImageFile, testStepResult.getScreenshotUrl(),
+                    appiumDriver, logger);
+
+            setSuccessMessage(successMessage);
+
+            // Switch back to old context
+            appiumDriver.execute(DriverCommand.SWITCH_TO_CONTEXT, ImmutableMap.of("name", existingContext));
+
+            return Result.SUCCESS;
+        } catch (Exception e) {
+            appiumDriver.execute(DriverCommand.SWITCH_TO_CONTEXT, ImmutableMap.of("name", existingContext));
+            logger.info("Exception: " + ExceptionUtils.getStackTrace(e));
+            setErrorMessage("Exception occurred while performing click on image: " + e.getMessage());
+            return Result.FAILED;
+        }
+    }
+
+}
---
+++ image_based_actions/src/main/java/com/testsigma/addons/mobile_web/SearchImage.java
@@ -0,0 +1,34 @@
+package com.testsigma.addons.mobile_web;
+
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.OCR;
+import com.testsigma.sdk.annotation.TestData;
+import com.testsigma.sdk.annotation.TestStepResult;
+import lombok.Data;
+import org.openqa.selenium.NoSuchElementException;
+
+@Data
+@Action(actionText = "Verify if image image-file present in current-page",
+        description = "Verify if the given image is present in current page",
+        applicationType = ApplicationType.MOBILE_WEB)
+public class SearchImage extends com.testsigma.addons.web.SearchImage {
+    @TestData(reference = "image-file")
+    private com.testsigma.sdk.TestData testData1;
+
+    @OCR
+    private com.testsigma.sdk.OCR ocr;
+
+    @TestStepResult
+    private com.testsigma.sdk.TestStepResult testStepResult;
+
+    @Override
+    protected Result execute() throws NoSuchElementException {
+        super.setTestData1(testData1);
+        super.setOcr(ocr);
+        super.setTestStepResult(testStepResult);
+        return super.execute();
+    }
+
+}
