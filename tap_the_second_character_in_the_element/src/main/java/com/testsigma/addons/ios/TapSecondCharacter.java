7086591e39a3e09f85e902e92df16508a9004523
akhil-testsigma <appini.akhil@testsigma.com>
2025-03-13 04:28:55 +0530

    Tap the second character in the element

---
+++ tap_the_second_character_in_the_element/src/main/java/com/testsigma/addons/ios/TapSecondCharacter.java
@@ -0,0 +1,56 @@
+package com.testsigma.addons.ios;
+
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.IOSAction;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.Element;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import lombok.Data;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+@Data
+@Action(actionText = "Tap the second character in the element element-locator",
+        applicationType = ApplicationType.IOS,
+        useCustomScreenshot = false)
+public class TapSecondCharacter extends IOSAction {
+
+  @Element(reference = "element-locator")
+  private com.testsigma.sdk.Element element;
+
+  @Override
+  protected com.testsigma.sdk.Result execute() throws NoSuchElementException {
+    com.testsigma.sdk.Result result = com.testsigma.sdk.Result.SUCCESS;
+    logger.info("Initiating execution");
+    logger.debug("Element Locator: "+ this.element.getValue() +" by:"+ this.element.getBy());
+    IOSDriver iosDriver = (IOSDriver)this.driver;
+    WebElement webElement = element.getElement();
+    if(webElement.isDisplayed()){
+      logger.info("Element is displayed, tapping the second character");
+      try {
+        int elementLeft = webElement.getLocation().getX();
+        int elementTop = webElement.getLocation().getY();
+        int elementWidth = webElement.getSize().getWidth();
+        int elementHeight = webElement.getSize().getHeight();
+
+        int y_offset = elementTop + (int)(elementHeight * 0.2);
+        int x_offset = elementLeft + (int)(elementWidth * 0.1);
+
+        TouchAction touchAction = new TouchAction(iosDriver);
+        touchAction.tap(PointOption.point(x_offset, y_offset)).perform();
+        setSuccessMessage("Successfully tapped the second character in the element");
+      } catch (Exception e) {
+        result = com.testsigma.sdk.Result.FAILED;
+        logger.warn("Failed to tap the second character in the element" + e);
+        setErrorMessage("Failed to tap the second character in the element: " + e.getMessage());
+      }
+    }else{
+      result = com.testsigma.sdk.Result.FAILED;
+      logger.warn("Element with locator %s is not visible ::"+element.getBy()+"::"+element.getValue());
+      setErrorMessage(String.format("Element with locator %s is not visible",element.getBy()));
+    }
+    return result;
+  }
+}
