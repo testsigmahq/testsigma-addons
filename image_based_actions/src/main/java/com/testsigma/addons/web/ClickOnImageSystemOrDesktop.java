6dcf081650e7a719fc670b1139fa832cd21899d3
akhil-testsigma <appini.akhil@testsigma.com>
2025-03-17 20:12:49 +0530

    added condition for checking whether it is cloud execution or not

--- image_based_actions/src/main/java/com/testsigma/addons/web/ClickOnImageSystemOrDesktop.java
+++ image_based_actions/src/main/java/com/testsigma/addons/web/ClickOnImageSystemOrDesktop.java
@@ -10,14 +10,17 @@ import com.testsigma.sdk.annotation.TestData;
 import com.testsigma.sdk.annotation.TestStepResult;
 import lombok.Data;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-
-;
 
 
 @Data
@@ -27,84 +30,106 @@ import java.io.File;
         useCustomScreenshot = true)
 
 public class ClickOnImageSystemOrDesktop extends WebAction {
-  @OCR
-  private com.testsigma.sdk.OCR ocr;
+    @OCR
+    private com.testsigma.sdk.OCR ocr;
 
-  @TestData(reference = "testdata")
-  private com.testsigma.sdk.TestData testData1;
+    @TestData(reference = "testdata")
+    private com.testsigma.sdk.TestData testData1;
 
-  @TestStepResult
-  private com.testsigma.sdk.TestStepResult testStepResult;
+    @TestStepResult
+    private com.testsigma.sdk.TestStepResult testStepResult;
 
-  @Override
-  protected Result execute() {
-    Result result = Result.SUCCESS;
-    try{
-      Robot robot = new Robot();
+    @Override
+    protected Result execute() {
+        Result result = Result.SUCCESS;
+        try {
+            if (isCloudExecution()) {
+                setErrorMessage("This action (Click on image testdata on system/desktop window) is not supported in cloud execution environments.");
+                result = Result.FAILED;
+                return result;
+            }
 
-      // Fetch the Details of the Screen Size
-      Rectangle screenSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            Robot robot = new Robot();
 
-      // Take the Snapshot of the Screen
-      BufferedImage tmp = robot.createScreenCapture(screenSize);
+            // Fetch the Details of the Screen Size
+            Rectangle screenSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 
-      // Provide the destination details to copy the screenshot
-      String tempDir = System.getProperty("java.io.tmpdir");
-      String filename = "screenshot"+System.currentTimeMillis()+".jpg";
-      String path = tempDir + filename;
-      logger.info("Path: " + path);
+            // Take the Snapshot of the Screen
+            BufferedImage tmp = robot.createScreenCapture(screenSize);
 
-      // To copy source image in to destination path
-      ImageIO.write(tmp, "jpg",new File(path));
-      int width = tmp.getWidth();
-      int height = tmp.getHeight();
-      logger.info("Width of image: " + width);
-      logger.info("Height of image: " + height);
+            // Provide the destination details to copy the screenshot
+            String tempDir = System.getProperty("java.io.tmpdir");
+            String filename = "screenshot" + System.currentTimeMillis() + ".jpg";
+            String path = tempDir + filename;
+            logger.info("Path: " + path);
 
-      File baseImageFile = new File(path);
-      String url = testStepResult.getScreenshotUrl();
-      logger.info("Amazon s3 url in which we are storing base image"+url);
-      ocr.uploadFile(url, baseImageFile);
-      logger.info("url: "+ testStepResult.getScreenshotUrl());
-      FindImageResponse responseObject = ocr.findImage(testData1.getValue().toString());
+            // To copy source image in to destination path
+            ImageIO.write(tmp, "jpg", new File(path));
+            int width = tmp.getWidth();
+            int height = tmp.getHeight();
+            logger.info("Width of image: " + width);
+            logger.info("Height of image: " + height);
 
-      if (responseObject.getIsFound()) {
-        boolean isFound = responseObject.getIsFound();
-        logger.info("Image found : " + isFound);
-        int x1 = responseObject.getX1();
-        int y1 = responseObject.getY1();
-        int x2 = responseObject.getX2();
-        int y2 = responseObject.getY2();
-        clickOnCoordinates(x1, y1, x2, y2, width, height);
-        setSuccessMessage("Image Found :" + isFound +
-                "    Image coordinates :" + "x1-" + x1 + ", x2-" + x2 + ", y1-" + y1 + ", y2-" + y2);
-        Thread.sleep(2000);
+            File baseImageFile = new File(path);
+            String url = testStepResult.getScreenshotUrl();
+            logger.info("Amazon s3 url in which we are storing base image" + url);
+            ocr.uploadFile(url, baseImageFile);
+            logger.info("url: " + testStepResult.getScreenshotUrl());
+            FindImageResponse responseObject = ocr.findImage(testData1.getValue().toString());
 
-      } else {
-        setErrorMessage("Unable to fetch the coordinates");
-        result = Result.FAILED;
-      }
+            if (responseObject.getIsFound()) {
+                boolean isFound = responseObject.getIsFound();
+                logger.info("Image found : " + isFound);
+                int x1 = responseObject.getX1();
+                int y1 = responseObject.getY1();
+                int x2 = responseObject.getX2();
+                int y2 = responseObject.getY2();
+                clickOnCoordinates(x1, y1, x2, y2, width, height);
+                setSuccessMessage("Image Found :" + isFound +
+                        "    Image coordinates :" + "x1-" + x1 + ", x2-" + x2 + ", y1-" + y1 + ", y2-" + y2);
+                Thread.sleep(2000);
+
+            } else {
+                setErrorMessage("Unable to fetch the coordinates");
+                result = Result.FAILED;
+            }
 
 
-    } catch (Exception e){
-      logger.info("Exception: "+ ExceptionUtils.getStackTrace(e));
-      setErrorMessage("Exception occurred while searching for the given image");
-      result = Result.FAILED;
+        } catch (Exception e) {
+            logger.info("Exception: " + ExceptionUtils.getStackTrace(e));
+            setErrorMessage("Exception occurred while searching for the given image: " + e.getMessage()); // Include the exception message.
+            result = Result.FAILED;
+        }
+
+
+        return result;
     }
 
 
-    return result;
-  }
+    public void clickOnCoordinates(int x1, int y1, int x2, int y2, int imageWidth, int imageHeight) throws AWTException {
+        Robot robot = new Robot();
 
+        int x = (x1 + x2) / 2;
+        int y = (y1 + y2) / 2;
 
-  public void clickOnCoordinates(int x1, int y1, int x2, int y2, int imageWidth, int imageHeight) throws AWTException {
-    Robot robot = new Robot();
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
 
-    int x = (x1 + x2) / 2;
-    int y = (y1 + y2) / 2;
-
-    robot.mouseMove(x, y);
-    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-  }
+    private boolean isCloudExecution() {
+        try {
+            // If the driver is an instance of any driver than it is local
+            if (driver instanceof ChromiumDriver || driver instanceof EdgeDriver || driver instanceof FirefoxDriver || driver instanceof SafariDriver) {
+                return false;
+            } else {
+                //It's a cloud environment
+                return true;
+            }
+        } catch (Exception e) {
+            logger.info("Exception: " + ExceptionUtils.getStackTrace(e));
+            setErrorMessage("Exception occurred while checking for cloud execution: " + e.getMessage());
+            return false;
+        }
+    }
 }
