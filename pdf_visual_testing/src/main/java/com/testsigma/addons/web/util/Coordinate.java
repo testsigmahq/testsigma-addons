5f9d745f72ef9da115bf30015678a8b3682fec32
ManojTestsigma <manojkumar.darshankar@testsigma.com>
2025-03-20 14:05:46 +0530

    [CR-2008] PDF visual testing added 2 Nlp's to get page count and visual diff at given page. (#128)
    * [CR-2008] added 2 Nlp's to get page count and visual diff at given page.
    
    * updated the condition

---
+++ pdf_visual_testing/src/main/java/com/testsigma/addons/web/util/Coordinate.java
@@ -0,0 +1,11 @@
+package com.testsigma.addons.web.util;
+
+import lombok.Data;
+
+@Data
+public class Coordinate {
+    private int x;
+    private int y;
+    private int w;
+    private int h;
+}
---
+++ pdf_visual_testing/src/main/java/com/testsigma/addons/web/util/CoordinateListDeserializer.java
@@ -0,0 +1,20 @@
+package com.testsigma.addons.web.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CoordinateListDeserializer extends JsonDeserializer<List<Coordinate>> {
+    @Override
+    public List<Coordinate> deserialize(JsonParser p, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String coordinatesJson = p.getText();
+        return mapper.readValue(coordinatesJson, mapper.getTypeFactory().constructCollectionType(List.class, Coordinate.class));
+    }
+}
