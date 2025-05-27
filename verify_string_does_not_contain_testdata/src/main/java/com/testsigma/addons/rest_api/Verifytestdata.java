8240913c8a5d25536dedff8a629c31e1870545af
akhil-testsigma <appini.akhil@testsigma.com>
2025-05-27 19:54:37 +0530

    Added nlp to rest api application

---
+++ verify_string_does_not_contain_testdata/src/main/java/com/testsigma/addons/rest_api/Verifytestdata.java
@@ -0,0 +1,48 @@
+package com.testsigma.addons.rest_api;
+
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.WebAction;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.TestData;
+import lombok.Data;
+import org.openqa.selenium.NoSuchElementException;
+
+@Data
+@Action(actionText = "Verify that the testdata does not contains testdata1",
+        description = "Verify that the testdata does not contains testdata",
+        applicationType = ApplicationType.REST_API)
+public class Verifytestdata extends WebAction {
+
+  @TestData(reference = "testdata")
+  private com.testsigma.sdk.TestData testData1;
+  
+  @TestData(reference = "testdata1")
+  private com.testsigma.sdk.TestData testData2;
+  
+  @Override
+  public Result execute() throws NoSuchElementException {
+    //Your Awesome code starts here
+    logger.info("Initiating execution");
+    Result result = Result.SUCCESS;
+  
+    	logger.debug("testdata => " + testData1.getValue().toString());
+        System.out.println("testdata => " + testData1.getValue().toString());
+        logger.debug("testdata1 => " + testData2.getValue().toString());
+        System.out.println("testdata1 => " + testData2.getValue().toString());
+         
+        String str = testData1.getValue().toString();
+ 	    
+        if (str.contains(testData2.getValue().toString()))
+ 	    {	
+        	 setErrorMessage(testData1.getValue().toString() + " contains " + testData2.getValue().toString());
+        	 return Result.FAILED;
+        } 
+        else 
+        {	
+        	setSuccessMessage(testData1.getValue().toString() + " does not contains " + testData2.getValue().toString());
+        }
+ 		
+    return result;
+  }
+}
