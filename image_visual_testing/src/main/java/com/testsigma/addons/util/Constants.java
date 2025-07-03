931e9429ac8f994311efb615bdb85aae5dd0a1ac
Manoj <manojkumar.darshankar@testsigma.com>
2025-07-03 14:48:11 +0530

    removed api token

--- image_visual_testing/src/main/java/com/testsigma/addons/util/Constants.java
+++ image_visual_testing/src/main/java/com/testsigma/addons/util/Constants.java
@@ -2,5 +2,5 @@ package com.testsigma.addons.util;
 
 public class Constants {
     public static String VISUAL_SERVER_API_END_POINT = "https://visualtesting-staging.testsigma.com/image_analysis_with_files";
-    public static String API_TOKEN = "DHRNEYFTDCDGOEDDCOEICVYOEEUY";
+    public static String API_TOKEN = "<api-token>";
 }
