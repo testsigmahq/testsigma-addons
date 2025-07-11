95e472f62d6398973ac7677b1e61b9e9b16355c1
Appini Akhil <appini.akhil@testsigma.com>
2025-07-11 14:00:21 +0530

    Added new class to verify if the image is present on current page

---
+++ verify_the_image_not_present_on_current_page/src/main/java/com/testsigma/addons/ios/VerifyImageNotPresentOnScreenWithThreshold.java
@@ -0,0 +1,71 @@
+package com.testsigma.addons.ios;
+
+import com.testsigma.sdk.*;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.OCR;
+import com.testsigma.sdk.annotation.TestData;
+import com.testsigma.sdk.annotation.TestStepResult;
+import io.appium.java_client.ios.IOSDriver;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+
+@Data
+@Action(actionText = "Verify if image image-url is not present in current-page with search threshold threshold-value (Ex: 0.9 , means 90% match)",
+        description = "Verify if the given image with threshold and scale is not present in the current page",
+        applicationType = ApplicationType.IOS,
+        useCustomScreenshot = true)
+public class VerifyImageNotPresentOnScreenWithThreshold extends IOSAction {
+    @TestData(reference = "image-url")
+    private com.testsigma.sdk.TestData imageURL;
+    @TestData(reference = "threshold-value")
+    private com.testsigma.sdk.TestData thresholdValue;
+
+    @OCR
+    private com.testsigma.sdk.OCR ocr;
+
+    @TestStepResult
+    private com.testsigma.sdk.TestStepResult testStepResult;
+
+    @Override
+    protected Result execute() throws NoSuchElementException {
+
+        logger.info("Initiating execution");
+        Result result = Result.SUCCESS;
+        try {
+            IOSDriver driver = (IOSDriver) this.driver;
+            String imageURL_ = imageURL.getValue().toString();
+            logger.info("Taking screenshot");
+            File baseImageFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            logger.info("Screenshot taken :" + baseImageFile.getAbsolutePath());
+            logger.info("Taking screenshot completed");
+            String url = testStepResult.getScreenshotUrl();
+            logger.info("Amazon s3 url in which we are storing base image"+url);
+            ocr.uploadFile(url, baseImageFile);
+            FindImageResponse response = ocr.findImage(imageURL_,Float.valueOf(thresholdValue.getValue().toString()));
+            logger.info("response :" + response);
+            if (response == null) {
+                logger.info("Given image not found in the current page");
+                setSuccessMessage("Given image not found in the current page");
+            } else {
+                logger.warn("Image Found :" + response.getIsFound() +
+                        "     Image coordinates :" + "x1-" + response.getX1() + ", x2-" + response.getX2() + ", y1-" + response.getY1() + ", y2-" + response.getY2());
+                setErrorMessage("Image Found :" + response.getIsFound() +
+                        "     Image coordinates :" + "x1-" + response.getX1() + ", x2-" + response.getX2() + ", y1-" + response.getY1() + ", y2-" + response.getY2());
+
+                result = Result.FAILED;
+            }
+
+        } catch (Exception e) {
+            result = Result.FAILED;
+            logger.warn("Exception: " + ExceptionUtils.getStackTrace(e));
+            setErrorMessage("Exception: " + ExceptionUtils.getMessage(e));
+        }
+
+        return result;
+    }
+}
