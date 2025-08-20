f50d346f0b3358ebb8680a81431012b93fe355f7
akhil-testsigma <appini.akhil@testsigma.com>
2025-08-20 22:43:11 +0530

    Added all ready present class to other applications

---
+++ store_string_lengths_split_by_a_delimiter/src/main/java/com/testsigma/addons/web/MyFirstWebAction.java
@@ -0,0 +1,64 @@
+package com.testsigma.addons.web;
+
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.WebAction;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.RunTimeData;
+import com.testsigma.sdk.annotation.TestData;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchElementException;
+
+@Data
+@Action(actionText = "Split the string testdata by delimeter and store the count of string into a runtime variable",
+        description = "This addon will Split the string testdata by delimeter and store the count of string into a runtime variable",
+        applicationType = ApplicationType.WEB,
+        useCustomScreenshot = false)
+public class MyFirstWebAction extends WebAction {
+
+    @TestData(reference = "testdata")
+    private com.testsigma.sdk.TestData testData1;
+
+    @TestData(reference = "delimeter")
+    private com.testsigma.sdk.TestData testData2;
+
+    @TestData(reference = "variable", isRuntimeVariable = true)
+    private com.testsigma.sdk.TestData runtimeVar;
+
+    @RunTimeData
+    private com.testsigma.sdk.RunTimeData runTimeData;
+
+    @Override
+    public Result execute() throws NoSuchElementException
+    {
+        logger.info("Initiating execution");
+        Result result = Result.SUCCESS;
+
+        try
+        {
+            logger.debug("testdata => " + testData1.getValue().toString());
+            logger.debug("delimeter => " + testData2.getValue().toString());
+            logger.debug("Runtime_Variable => " + runtimeVar.getValue().toString());
+
+            String ABS = testData1.getValue().toString();
+            String[] strings = ABS.split(testData2.getValue().toString());
+            int res = strings.length;
+            logger.info("res " + res);
+
+            runTimeData.setKey(runtimeVar.getValue().toString());
+            runTimeData.setValue(String.valueOf(res));
+
+            logger.info("Successfully stored the count of the given string " + testData1.getValue().toString() + " into a runtime variable " + runtimeVar.getValue().toString() + " = " + res);
+            setSuccessMessage("Successfully stored the count of the given string " + testData1.getValue().toString() + " into a runtime variable " + runtimeVar.getValue().toString() + " = " + res);
+
+        }
+        catch(Exception e)
+        {
+            logger.warn("Exception while executing action - "+ ExceptionUtils.getStackTrace(e));
+            setErrorMessage("Operation failed , the error message is ::::"+ExceptionUtils.getMessage(e));
+            result = Result.FAILED;
+        }
+        return result;
+    }
+}
