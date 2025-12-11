19bfff46b2fb88f21bf1598504912cdec06e9514
Akhil Appini <appini.akhil@testsigma.com>
2025-12-11 15:33:27 +0530

    Migrated the addon to IN region

---
+++ trim_white_spaces_from_testdata/src/main/java/com/testsigma/addons/ios/TrimWhiteSpaces.java
@@ -0,0 +1,60 @@
+package com.testsigma.addons.ios;
+
+import com.testsigma.sdk.IOSAction;
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.RunTimeData;
+import com.testsigma.sdk.annotation.TestData;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchElementException;
+
+@Data
+@Action(actionText = "Trim white space from testdata and store it in a runtime variable",
+        description = "This addon will trim all the white spaces from the given string and stores it in a runtime variable.",
+        applicationType = ApplicationType.IOS)
+
+public class TrimWhiteSpaces extends IOSAction {
+	
+  @TestData(reference = "testdata")
+  private com.testsigma.sdk.TestData testdata;
+  
+  @TestData(reference = "variable", isRuntimeVariable = true)
+  private com.testsigma.sdk.TestData runtimeVar;
+  
+  @RunTimeData
+  private com.testsigma.sdk.RunTimeData runTimeData;
+
+  @Override
+  public Result execute() throws NoSuchElementException {
+    //Your Awesome code starts here
+    logger.info("Initiating execution");
+    Result result = Result.SUCCESS;
+    try 
+    {
+    	String input = testdata.getValue().toString();
+    	if (input.isEmpty()) 
+    	{
+    		result = Result.FAILED;
+    		setErrorMessage("Given testdata is empty");
+			
+		} else 
+		{
+			String Output = input.replaceAll("\\s", "");
+            logger.info("Output: " + Output);
+
+    		runTimeData.setKey(runtimeVar.getValue().toString());
+    		runTimeData.setValue(Output);
+           
+    		result = Result.SUCCESS;
+    		setSuccessMessage("Successfully trimmed the whitespace from the given string and store in a runtime variable " + runtimeVar.getValue().toString() +" = "+ Output);
+		}
+    } catch (Exception e) {
+        result = Result.FAILED;
+        logger.warn("Exception Occurred: " + ExceptionUtils.getStackTrace(e));
+        setErrorMessage("Exception Occurred: " + ExceptionUtils.getMessage(e));
+	}
+    return result;
+  }
+}
