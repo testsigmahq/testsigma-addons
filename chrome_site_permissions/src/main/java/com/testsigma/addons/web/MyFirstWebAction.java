dfb0e7f9f36d2ec5a3779a4f9bcccd827b8662d7
Akhil Appini <appini.akhil@testsigma.com>
2026-01-20 14:45:06 +0530

    Added support for the latest chrome permissions

--- chrome_site_permissions/src/main/java/com/testsigma/addons/web/MyFirstWebAction.java
+++ chrome_site_permissions/src/main/java/com/testsigma/addons/web/MyFirstWebAction.java
@@ -16,12 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@Action(
-        actionText = "Set chrome permission to URL: Target-URL , Permission Name: Permission-Name, Permission Type: Permission-Value",
-        description = "Set chrome site settings permissions to URL.",
-        applicationType = ApplicationType.WEB,
-        useCustomScreenshot = false
-)
+@Action(actionText = "Set chrome permission to URL: Target-URL , Permission Name: Permission-Name, Permission Type: Permission-Value", description = "Set chrome site settings permissions to URL.", applicationType = ApplicationType.WEB, useCustomScreenshot = false)
 public class MyFirstWebAction extends WebAction {
 
     @TestData(reference = "Target-URL")
@@ -59,16 +54,33 @@ public class MyFirstWebAction extends WebAction {
 
             // Determine which permission selector to use
             Boolean isOldStructure = (Boolean) js.executeScript(
-                    "return !!document.querySelector('body > settings-ui')?.shadowRoot.querySelector('#main')?.shadowRoot.querySelector('settings-basic-page');"
-            );
+                    "return !!document.querySelector('body > settings-ui')?.shadowRoot.querySelector('#main')?.shadowRoot.querySelector('settings-basic-page');");
 
+            logger.info("Is old structure flag: " + isOldStructure);
+            Boolean isChrome143Structure = (Boolean) js.executeScript(
+                    "return !!document.querySelector('body > settings-ui')?.shadowRoot.querySelector('#main')?.shadowRoot.querySelector('settings-privacy-page-index')?.shadowRoot.querySelector('#siteSettingsSiteDetails');");
+
+            logger.info("Is Chrome 143+ structure: " + isChrome143Structure);
             String permissionSelect;
-            if (isOldStructure) {
-                permissionSelect = "return document.querySelector(\"body > settings-ui\").shadowRoot.querySelector(\"#main\").shadowRoot" +
-                        ".querySelector(\"settings-basic-page\").shadowRoot.querySelector(\"#basicPage > settings-section.expanded > settings-privacy-page\").shadowRoot" +
+
+            if (isChrome143Structure) {
+                logger.info("Its Chrome 143+ Structure");
+                permissionSelect = "return document.querySelector(\"body > settings-ui\")\n" +
+                        "  .shadowRoot.querySelector(\"#main\")\n" +
+                        "  .shadowRoot.querySelector(\"settings-privacy-page-index\")\n" +
+                        "  .shadowRoot.querySelector(\"#siteSettingsSiteDetails\")\n" +
+                        "  .shadowRoot.querySelector(\"site-details-permission[label='%s']\")\n" +
+                        "  .shadowRoot.querySelector(\"#permission\")";
+            } else if (isOldStructure) {
+                logger.info("Its Old Structure");
+                permissionSelect = "return document.querySelector(\"body > settings-ui\").shadowRoot.querySelector(\"#main\").shadowRoot"
+                        +
+                        ".querySelector(\"settings-basic-page\").shadowRoot.querySelector(\"#basicPage > settings-section.expanded > settings-privacy-page\").shadowRoot"
+                        +
                         ".querySelector(\"#pages > settings-subpage > site-details\").shadowRoot" +
                         ".querySelector(\"div.list-frame > site-details-permission[label='%s']\").shadowRoot.querySelector(\"#permission\")";
             } else {
+                logger.info("Its New Structure (pre-143)");
                 permissionSelect = "return document.querySelector(\"body > settings-ui\")\n" +
                         "  .shadowRoot.querySelector(\"#main\")\n" +
                         "  .shadowRoot.querySelector(\"#privacy > settings-privacy-page-index\")\n" +
@@ -96,7 +108,7 @@ public class MyFirstWebAction extends WebAction {
         } catch (Exception error) {
             result = com.testsigma.sdk.Result.FAILED;
             logger.info("Exception: " + ExceptionUtils.getStackTrace(error));
-            setErrorMessage("Unable to change site permissions, " + ExceptionUtils.getMessage(error));
+            setErrorMessage("Unable to change site permissions: " + ExceptionUtils.getMessage(error));
             return result;
         } finally {
             driver.switchTo().window(origWindowHandle);
