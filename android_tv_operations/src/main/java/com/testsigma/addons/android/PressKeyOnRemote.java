4da2055a7982535850a265ffbb6270e574d1b14b
Akhil Appini <appini.akhil@testsigma.com>
2026-02-03 00:01:29 +0530

    Fix Android TV key press (mobile: pressKey + keycode)

--- android_tv_operations/src/main/java/com/testsigma/addons/android/PressKeyOnRemote.java
+++ android_tv_operations/src/main/java/com/testsigma/addons/android/PressKeyOnRemote.java
@@ -5,10 +5,11 @@ import com.testsigma.sdk.ApplicationType;
 import com.testsigma.sdk.Result;
 import com.testsigma.sdk.annotation.Action;
 import com.testsigma.sdk.annotation.TestData;
+import io.appium.java_client.CommandExecutionHelper;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
 import lombok.Data;
+
+import java.util.Map;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.openqa.selenium.NoSuchElementException;
 
@@ -19,18 +20,18 @@ import org.openqa.selenium.NoSuchElementException;
 public class PressKeyOnRemote extends AndroidAction {
 
     @TestData(
-                reference = "key-value",
-                allowedValues =
+            reference = "key-value",
+            allowedValues =
                     {
-                        "Up", "Down", "Left", "Right", "Guide", "Tv-contents-menu",
-                        "Menu", "Info", "Ch+", "Ch-", "Back", "Ok",
-                        "NUM_0", "NUM_1", "NUM_2", "NUM_3", "NUM_4", "NUM_5",
-                        "NUM_6", "NUM_7", "NUM_8", "NUM_9", "Volume-Up", "Volume-Down",
-                        "PlayPause", "Rewind", "Forward", "Home", "Power", "App-switch",
-                        "Soft-left", "Soft-right", "Navigate-previous", "Navigate-next", "Navigate-in", "Navigate-out",
-                        "Stem-1-Netflix", "Stem-2", "Stem-3"
+                            "Up", "Down", "Left", "Right", "Guide", "Tv-contents-menu",
+                            "Menu", "Info", "Ch+", "Ch-", "Back", "Ok",
+                            "NUM_0", "NUM_1", "NUM_2", "NUM_3", "NUM_4", "NUM_5",
+                            "NUM_6", "NUM_7", "NUM_8", "NUM_9", "Volume-Up", "Volume-Down",
+                            "PlayPause", "Rewind", "Forward", "Home", "Power", "App-switch",
+                            "Soft-left", "Soft-right", "Navigate-previous", "Navigate-next", "Navigate-in", "Navigate-out",
+                            "Stem-1-Netflix", "Stem-2", "Stem-3"
                     }
-             )
+    )
     private com.testsigma.sdk.TestData key;
 
     @Override
@@ -38,9 +39,9 @@ public class PressKeyOnRemote extends AndroidAction {
         Result result = Result.SUCCESS;
         try {
             logger.info("Initiating execution");
-            AndroidDriver androidDriver = (AndroidDriver)this.driver;
-            AndroidKey androidKey = KeyUtil.getKey(key.getValue().toString());
-            androidDriver.pressKey(new KeyEvent(androidKey));
+            AndroidDriver androidDriver = (AndroidDriver) this.driver;
+            int keycode = KeyUtil.getKeyCode(key.getValue().toString());
+            CommandExecutionHelper.executeScript(androidDriver, "mobile: pressKey", Map.of("keycode", keycode));
             setSuccessMessage("Pressed the key successfully");
         } catch (IllegalArgumentException e) {
             logger.info("Invalid key value");
