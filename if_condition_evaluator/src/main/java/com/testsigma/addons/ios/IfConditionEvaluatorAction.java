cd469247ca05e79b9c2b2bc1d5a992b25aa52f31
Akhil Appini <appini.akhil@testsigma.com>
2026-03-24 00:10:05 +0530

    Added utils to support bracket conditons

--- if_condition_evaluator/src/main/java/com/testsigma/addons/ios/IfConditionEvaluatorAction.java
+++ if_condition_evaluator/src/main/java/com/testsigma/addons/ios/IfConditionEvaluatorAction.java
@@ -1,25 +1,16 @@
 package com.testsigma.addons.ios;
 
+import com.testsigma.addons.utils.ConditionEvaluatorUtils;
 import com.testsigma.sdk.ApplicationType;
 import com.testsigma.sdk.IOSAction;
 import com.testsigma.sdk.StepActionType;
-import com.testsigma.sdk.WebAction;
 import com.testsigma.sdk.annotation.Action;
 import com.testsigma.sdk.annotation.TestData;
 import lombok.Data;
 import org.openqa.selenium.NoSuchElementException;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-
 @Data
-@Action(
-        actionText = "Verify if testdata is satisfied (Eg: 5 > 3 OR 4 > 5 AND Testsigma CONTAINS sigma)",
-        description = "Evaluates arithmetic (+, -, *, /), logical (AND, OR), comparison (>, <, >=, <=, ==, !=), and string conditions (CONTAINS, NOT CONTAINS) dynamically within IF steps.",
-        applicationType = ApplicationType.IOS,
-        actionType = StepActionType.IF_CONDITION,
-        useCustomScreenshot = false
-)
+@Action(actionText = "Verify if testdata is satisfied (Eg: 5 > 3 OR 4 > 5 AND Testsigma CONTAINS sigma; X CONTAINS (A OR B) or X CONTAINS (A AND B))", description = "Evaluates arithmetic (+, -, *, /), logical (AND, OR), comparison (>, <, >=, <=, ==, !=), and string conditions (CONTAINS, NOT CONTAINS) dynamically within IF steps. CONTAINS (A OR B) expands to CONTAINS A OR CONTAINS B; CONTAINS (A AND B) expands to CONTAINS A AND CONTAINS B.", applicationType = ApplicationType.IOS, actionType = StepActionType.IF_CONDITION, useCustomScreenshot = false)
 public class IfConditionEvaluatorAction extends IOSAction {
 
   @TestData(reference = "testdata")
@@ -39,8 +30,8 @@ public class IfConditionEvaluatorAction extends IOSAction {
 
       String condition = testData.getValue().toString().trim();
       logger.info("Original Condition: " + condition);
-
-      boolean finalResult = evaluateCondition(condition);
+      logger.info("Converted Condition (JS format): " + ConditionEvaluatorUtils.toJavaScriptExpression(condition));
+      boolean finalResult = ConditionEvaluatorUtils.evaluateCondition(condition);
       logger.info("Final Evaluated Result: " + finalResult);
 
       if (!finalResult) {
@@ -60,44 +51,4 @@ public class IfConditionEvaluatorAction extends IOSAction {
     return result;
   }
 
-  private boolean evaluateCondition(String condition) throws Exception {
-
-    // Convert logical operators
-    condition = condition.replaceAll("(?i)\\bAND\\b", "&&");
-    condition = condition.replaceAll("(?i)\\bOR\\b", "||");
-
-    String operandPattern = "([a-zA-Z0-9_\\.]+|\"[^\"]*\"|'[^']*')";
-
-    // Convert NOT CONTAINS
-    condition = condition.replaceAll(
-            "(?i)" + operandPattern + "\\s+NOT\\s+CONTAINS\\s+" + operandPattern,
-            "String($1).indexOf(String($2)) === -1"
-    );
-
-    // Convert CONTAINS
-    condition = condition.replaceAll(
-            "(?i)" + operandPattern + "\\s+CONTAINS\\s+" + operandPattern,
-            "String($1).indexOf(String($2)) !== -1"
-    );
-
-    logger.info("Converted Condition (JS format): " + condition);
-
-    ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
-
-    if (engine == null) {
-      throw new Exception("JavaScript Engine not available. Check Java version.");
-    }
-
-    Object evalResult = engine.eval(condition);
-
-    if (evalResult instanceof Boolean) {
-      return (Boolean) evalResult;
-    }
-
-    if (evalResult instanceof Number) {
-      return ((Number) evalResult).doubleValue() != 0;
-    }
-
-    return false;
-  }
 }
