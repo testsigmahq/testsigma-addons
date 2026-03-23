476b8ef27b09db714419d6b3fb241931a6bbaadc
ManojTestsigma <manojkumar.darshankar@testsigma.com>
2026-03-23 21:52:27 +0530

    [CUS-11475] Added existing nlp to all application types.

---
+++ add_minutes_to_the_specific_date_time_and_store_the_output_in_specific_format/src/main/java/com/testsigma/addons/ios/AddMinutesToDateTime.java
@@ -0,0 +1,116 @@
+package com.testsigma.addons.ios;
+
+import com.testsigma.sdk.IOSAction;
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.TestData;
+import com.testsigma.sdk.annotation.RunTimeData;
+import lombok.Data;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.openqa.selenium.NoSuchElementException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+@Data
+@Action(actionText = "Add minute minutes to the input-datetime with input-datetime-format, convert to output-datetime-format format, and store it in a runtime variable variable-name",
+        description = "Adds specified minutes to a given input datetime, converts the resulting datetime to a specified date-time format, and stores it in a runtime variable",
+        applicationType = ApplicationType.IOS,
+        useCustomScreenshot = false)
+public class AddMinutesToDateTime extends IOSAction {
+
+  @TestData(reference = "input-datetime")
+  private com.testsigma.sdk.TestData dateTime;
+  @TestData(reference = "input-datetime-format")
+  private com.testsigma.sdk.TestData inputFormat;
+  @TestData(reference = "output-datetime-format")
+  private com.testsigma.sdk.TestData outputFormat;
+  @TestData(reference = "minute")
+  private com.testsigma.sdk.TestData minutesToAdd;
+
+  @TestData(reference = "variable-name", isRuntimeVariable = true)
+  private com.testsigma.sdk.TestData variableName;
+
+  @RunTimeData
+  private com.testsigma.sdk.RunTimeData runTimeData;
+
+  @Override
+  public com.testsigma.sdk.Result execute() throws NoSuchElementException {
+    logger.info("Initiating execution");
+    com.testsigma.sdk.Result result = com.testsigma.sdk.Result.SUCCESS;
+    String dateTimeString = dateTime.getValue().toString();
+    String inputFormatString = inputFormat.getValue().toString();
+    String outputFormatString = outputFormat.getValue().toString();
+    String minutesToAddString = minutesToAdd.getValue().toString();
+    String variableNameString = variableName.getValue().toString();
+
+    try {
+      int minutes = Integer.parseInt(minutesToAddString);
+
+      if (minutes < 0) {
+        result = com.testsigma.sdk.Result.FAILED;
+        setErrorMessage("The 'minute' value must be a non-negative integer.");
+        logger.warn("Invalid input: 'minute' value is negative.");
+        return result;
+      }
+
+      String newDateTime = addMinutesToDateTime(dateTimeString, inputFormatString, outputFormatString, minutes);
+
+      logger.info("New date-time value: " + newDateTime);
+
+      if (newDateTime != null) {
+        runTimeData.setValue(newDateTime);
+        runTimeData.setKey(variableNameString);
+        logger.info("Final value stored in variable '" + variableNameString + "' with value '" + newDateTime + "'");
+        setSuccessMessage(String.format("Added %s minutes to %s and stored the output in '%s' format. %s : %s",
+                minutes, dateTimeString, outputFormatString, variableNameString, newDateTime));
+      } else {
+        result = com.testsigma.sdk.Result.FAILED;
+        setErrorMessage("Failed to add minutes. Please check the input datetime, input datetime format, and output datetime format.");
+      }
+
+    } catch (NumberFormatException e) {
+      result = com.testsigma.sdk.Result.FAILED;
+      setErrorMessage("Invalid number format for 'minutes': " + minutesToAddString);
+      logger.warn("NumberFormatException: " + e);
+
+    } catch (Exception e) {
+      result = com.testsigma.sdk.Result.FAILED;
+      setErrorMessage("An unexpected error occurred: " + ExceptionUtils.getMessage(e));
+      logger.warn("Exception during execution: " + ExceptionUtils.getStackTrace(e));
+    }
+    return result;
+  }
+
+  public String addMinutesToDateTime(String dateTimeString, String inputFormatString, String outputFormatString, int minutesToAdd) {
+    try {
+      DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern(inputFormatString);
+
+      if (inputFormatString.contains("HH") || inputFormatString.contains("mm") || inputFormatString.contains("ss")) {
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(outputFormatString);
+        LocalTime time = LocalTime.parse(dateTimeString, inputFormatter);
+        LocalTime newTime = time.plusMinutes(minutesToAdd);
+        logger.info("Successfully added minutes to the input time");
+
+        return newTime.format(outputFormatter);
+
+      } else {
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(outputFormatString);
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, inputFormatter);
+
+        LocalDateTime newDateTime = dateTime.plusMinutes(minutesToAdd);
+        logger.info("Successfully added minutes to the input datetime");
+
+        return newDateTime.format(outputFormatter);
+      }
+
+    } catch (DateTimeParseException e) {
+      String errorMessage = "Invalid date/time format. Please use the format: " + inputFormatString + ". Error: " + e.getMessage();
+      logger.warn(errorMessage);
+      setErrorMessage(errorMessage);
+      return null;
+    }
+  }
+}
