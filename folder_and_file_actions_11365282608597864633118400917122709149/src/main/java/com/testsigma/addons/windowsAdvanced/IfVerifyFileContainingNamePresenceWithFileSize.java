38bbc59e80f1d6274f8da740d93875f4559f2e4b
ManojTestsigma <manojkumar.darshankar@testsigma.com>
2026-04-29 12:06:05 +0530

    [CUS-12422] Added folder and file actions to windows advanced type.

---
+++ folder_and_file_actions_11365282608597864633118400917122709149/src/main/java/com/testsigma/addons/windowsAdvanced/IfVerifyFileContainingNamePresenceWithFileSize.java
@@ -0,0 +1,72 @@
+package com.testsigma.addons.windowsAdvanced;
+
+import com.testsigma.addons.web.folderutil.FolderUtilities;
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.StepActionType;
+import com.testsigma.sdk.WindowsAdvancedAction;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.TestData;
+import lombok.Data;
+
+import java.io.File;
+import java.util.NoSuchElementException;
+
+@Data
+@Action(actionText = "Verify that the folder Folder-Path has a file with name containing File-Name and the size of " +
+        "the file is greater than test-data KB",
+        description = "Verifies whether the folder has any file containing the given name and its size exceeds the " +
+                "given input (use in if condition step)",
+        applicationType = ApplicationType.WINDOWS_ADVANCED,
+        actionType = StepActionType.IF_CONDITION,
+        displayName = "If: Verify folder has file containing name and size",
+        useCustomScreenshot = false)
+public class IfVerifyFileContainingNamePresenceWithFileSize extends WindowsAdvancedAction {
+
+    @TestData(reference = "Folder-Path")
+    private com.testsigma.sdk.TestData folderPath_;
+
+    @TestData(reference = "File-Name")
+    private com.testsigma.sdk.TestData fileName_;
+
+    @TestData(reference = "test-data")
+    private com.testsigma.sdk.TestData size_;
+
+    @Override
+    protected Result execute() throws NoSuchElementException {
+        logger.info("Initiating execution...");
+        Result result = Result.SUCCESS;
+        String folderPath = folderPath_.getValue().toString();
+        String fileName = fileName_.getValue().toString();
+        long size = Long.parseLong(size_.getValue().toString());
+
+        logger.info("Given folder path : " + folderPath);
+        logger.info("Given file name : " + fileName);
+        logger.info("Given size value : " + size);
+
+        FolderUtilities util = new FolderUtilities();
+        if (util.folderCheck(folderPath)) {
+            File file = util.searchFile(new File(folderPath), fileName, false, logger);
+            if (file != null) {
+                long fileSize = file.length() / 1000;
+                if (file.length() > (size * 1000)) {
+                    setSuccessMessage(String.format("Successfully verified that file exists containing name %s in" +
+                                    " the folder path %s and it's size %s KB is greater than %s KB",
+                            fileName, folderPath, fileSize, size));
+                } else {
+                    result = Result.FAILED;
+                    setErrorMessage(String.format("The files exists with containing name %s in the folder path %s and its" +
+                            " size %s KB is not greater than %s KB", fileName, folderPath, fileSize, size));
+                }
+            } else {
+                result = Result.FAILED;
+                setErrorMessage(String.format(util.NO_FILE_CONTAINS_ERROR_MSG, fileName, folderPath));
+            }
+        } else {
+            result = Result.FAILED;
+            setErrorMessage(String.format(util.FOLDER_NOT_FOUND, folderPath));
+        }
+        logger.info("Execution completed");
+        return result;
+    }
+}
---
+++ folder_and_file_actions_11365282608597864633118400917122709149/src/main/java/com/testsigma/addons/windowsAdvanced/VerifyFileWithNamePresence.java
@@ -0,0 +1,55 @@
+package com.testsigma.addons.windowsAdvanced;
+
+import com.testsigma.addons.web.folderutil.FolderUtilities;
+import com.testsigma.sdk.ApplicationType;
+import com.testsigma.sdk.Result;
+import com.testsigma.sdk.WindowsAdvancedAction;
+import com.testsigma.sdk.annotation.Action;
+import com.testsigma.sdk.annotation.TestData;
+import lombok.Data;
+
+import java.io.File;
+import java.util.NoSuchElementException;
+
+@Data
+@Action(actionText = "Verify that the folder Folder-Path has a file with name File-Name",
+        description = "Verifies whether the folder has a file with given name",
+        applicationType = ApplicationType.WINDOWS_ADVANCED,
+        displayName = "Verify folder has file with exact name",
+        useCustomScreenshot = false)
+public class VerifyFileWithNamePresence extends WindowsAdvancedAction {
+
+    @TestData(reference = "Folder-Path")
+    private com.testsigma.sdk.TestData folderPath_;
+
+    @TestData(reference = "File-Name")
+    private com.testsigma.sdk.TestData fileName_;
+
+    @Override
+    protected Result execute() throws NoSuchElementException {
+        logger.info("Initiating execution...");
+        Result result = Result.SUCCESS;
+        String folderPath = folderPath_.getValue().toString();
+        String fileName = fileName_.getValue().toString();
+
+        logger.info("Given folder path : " + folderPath);
+        logger.info("Given file name : " + fileName);
+
+        FolderUtilities util = new FolderUtilities();
+        if (util.folderCheck(folderPath)) {
+            File file = util.searchFile(new File(folderPath), fileName, true, logger);
+            if (file != null) {
+                setSuccessMessage(String.format("Successfully verified that file exists with name %s in the folder " +
+                        "path %s", fileName, folderPath));
+            } else {
+                result = Result.FAILED;
+                setErrorMessage(String.format(util.NO_FILE_EXISTS_ERROR_MSG, fileName, folderPath));
+            }
+        } else {
+            result = Result.FAILED;
+            setErrorMessage(String.format(util.FOLDER_NOT_FOUND, folderPath));
+        }
+        logger.info("Execution completed");
+        return result;
+    }
+}
